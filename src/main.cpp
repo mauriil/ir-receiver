ee980db7b8a77adc91f391dd848431171201e150
Mauri Lahitte <mauricio..mm94@gmail.com>
2022-03-28 10:42:41 -0300

    adding v3 to sender and receiver

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
+// https://www.pjrc.com/teensy/td_libs_IRremote.html
+// https://github.com/Arduino-IRremote/Arduino-IRremote
 #include <IRremote.h>
 
 int RECV_PIN =7;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
+//IRsend irsend;
+
 
 void setup(){
  Serial.begin(9600);
- irrecv.enableIRIn();
+ irrecv.begin(RECV_PIN);
 }
 
 void loop(){
-   if (irrecv.decode(&results)){
-      int value = results.value;
-      Serial.print("Received: ");
-      Serial.println(value);
-      irrecv.resume();
-   }
+
+  if (IrReceiver.decode()) {
+     Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+     IrReceiver.printIRResultShort(&Serial); // optional use new print version
+     IrReceiver.resume(); // Enable receiving of the next value
+ }
+
+ //irsend.sendNEC(0xA, 0x5A, 2);
+ //delay(1000);
 }
